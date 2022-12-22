b398c6e2c8db4f20dd8e68f85dc4b67be353fe42
star-dark <74770897+star-dark@users.noreply.github.com>
2022-12-22 14:32:11 +0900

    Add files via upload

---
+++ NFT721.sol
@@ -0,0 +1,17 @@
+pragma solidity 0.8.7;
+
+import "https://github.com/0xcert/ethereum-erc721/src/contracts/tokens/nf-token-metadata.sol";
+import "https://github.com/0xcert/ethereum-erc721/src/contracts/ownership/ownable.sol";
+
+contract newNFT is NFTokenMetadata, Ownable {
+ constructor() {
+   //define nft name of choice and symbol
+   nftName = "SectionTest NFT";
+   nftSymbol = "STNF";
+ }
+ 
+ function mint(address _to, uint256 _tokenId, string calldata _uri) external onlyOwner {
+   super._mint(_to, _tokenId);
+   super._setTokenUri(_tokenId, _uri);
+ }
+}
